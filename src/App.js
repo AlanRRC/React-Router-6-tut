79bd448d424fd58d7eb1a1cd1f6ec8e7a0d928e2
Alan Simpson <asimpson@rrc.ca>
2022-06-14 13:50:08 -0500

    nested routes in RR6

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
-
+import { BrowserRouter, Link, Route, Routes, Navigate } from 'react-router-dom'
+import { useState } from 'react'
 // pages
 import Home from './pages/Home'
 import About from './pages/About'
 import Products from './pages/Products'
 import ProductDetails from './pages/ProductDetails'
 
+
 function App() {
+  const [cartIsEmpty] = useState(false)
+
   return (
     <div className="App">
       <BrowserRouter>
@@ -18,9 +21,14 @@ function App() {
         </nav>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/products/:id" element={<ProductDetails />} />
+          <Route path="/about/*" element={<About />} />
+          <Route path="/products/:id/*" element={<ProductDetails />} />
           <Route path="/products" element={<Products />} />
+          <Route path="/redirect" element={<Navigate to="/about" />} />
+          <Route
+            path='/checkout'
+            element={cartIsEmpty ? <Navigate to="/products" /> : <p>checkout</p>}
+          />
         </Routes>
       </BrowserRouter>
     </div>
